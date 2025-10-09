18d94d4a0b7a962cdd2c90773601a04150616ee8
taguchi <honoka.taguchi@carrot.co.jp>
2025-10-09 05:45:30 +0000

    Issue #2 User  フリガナのカラムを追加 名前をfirst とlast に分割

--- db/migrate/20251007041015_create_users.rb
+++ db/migrate/20251007041015_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name_kana, null: false
+      t.string :last_name_kana, null: false
       t.string :email, null: false
       t.string :password_digest, null: false
       t.references :role, null: false, foreign_key: true
