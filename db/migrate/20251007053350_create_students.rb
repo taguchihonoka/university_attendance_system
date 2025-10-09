4a0ca82cf6b4ddef33e0f76ab1414e76085d29ea
taguchi <honoka.taguchi@carrot.co.jp>
2025-10-09 05:53:04 +0000

    Issue #2 入学年と卒業年の型とカラム名を変更 faculty_id を削除 中退を追加
    yearだと、編入に対応できないため
    学部に紐づいていない学科で登録できないようにするため
    卒業できない人に対応するカラムがなかったため

--- db/migrate/20251007053350_create_students.rb
+++ db/migrate/20251007053350_create_students.rb
@@ -3,11 +3,12 @@ class CreateStudents < ActiveRecord::Migration[8.0]
     create_table :students do |t|
       t.references :user, null: false, foreign_key: true
       t.string :attendance_number, null: false
-      t.references :faculty, null: false, foreign_key: true
       t.references :department, null: false, foreign_key: true
       t.integer :grade, null: false
-      t.integer :enrollment_year, null: false
-      t.integer :graduation_year, null: false
+      t.date :enrolled_date, null: false
+      t.date :graduated_date
+      t.date :dropout_date
+
 
       t.timestamps
     end
