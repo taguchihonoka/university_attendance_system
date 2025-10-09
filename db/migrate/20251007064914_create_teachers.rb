72e42e5d550d1efe28bbf02db3b0fdee166e68ad
taguchi <honoka.taguchi@carrot.co.jp>
2025-10-09 06:01:30 +0000

    Issue #2 着任日、離任日の型とカラム名を変更 faculty_id を削除
    カラム名と入力データが合っていなかったため
    学部に紐ずいていない学科を登録できないようにするため

--- db/migrate/20251007064914_create_teachers.rb
+++ db/migrate/20251007064914_create_teachers.rb
@@ -3,11 +3,10 @@ class CreateTeachers < ActiveRecord::Migration[8.0]
     create_table :teachers do |t|
       t.references :user, null: false, foreign_key: true
       t.string :instructor_number, null: false
-      t.references :faculty, null: false, foreign_key: true
       t.references :department, null: false, foreign_key: true
       t.references :position, null:false, foreign_key: true
-      t.integer :arrival_year, null: false
-      t.integer :retirement_year
+      t.date :assigned_date, null: false
+      t.date :retired_date
 
       t.timestamps
     end
