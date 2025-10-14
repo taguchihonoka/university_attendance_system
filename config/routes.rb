978134f7323774f0df3f5ec5fc6e0991e4f8eaec
taguchi <honoka.taguchi@carrot.co.jp>
2025-10-14 02:34:42 +0000

    Isuue #5 bin/rails g controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
+  get "users/new"
+  get "users/edit"
+  get "users/update"
+  get "users/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
